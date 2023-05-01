c793d85991335073afc6def2937015f692d392c8
Mark Glen Sadang Miguel <markgmiguel@outlook.com>
2023-05-01 22:33:31 +0800

    Adding Checkout and Place Order Page using Livewire

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -41,4 +41,8 @@ class FrontendController extends Controller
             return redirect()->back();
         }
     }
+
+    public function thankyou(){
+        return view('frontend.thank-you');
+    }
 }
---
+++ resources/views/frontend/checkout/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Checkout - Cluckoo')
+
+@section('content')
+
+<livewire:frontend.checkout.checkout-show />
+
+@endsection
