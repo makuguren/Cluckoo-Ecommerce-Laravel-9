415d37faeda0ca47824491acadd21ecea260b098
Mark Glen Sadang Miguel <markgmiguel@outlook.com>
2023-05-05 19:38:02 +0800

    Added Users CRUD with Roles in Admin Interface

--- routes/web.php
+++ routes/web.php
@@ -93,4 +93,14 @@ Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function () {
         Route::get('/invoice/{orderId}/generate', 'generateInvoice');
     });
 
+    //Users Controller
+    Route::controller(App\Http\Controllers\Admin\UserController::class)->group(function () {
+        Route::get('/users', 'index');
+        Route::get('/users/create', 'create');
+        Route::post('/users', 'store');
+        Route::get('/users/{user_id}/edit', 'edit');
+        Route::put('users/{user_id}', 'update');
+        Route::get('users/{user_id}/delete', 'destroy');
+    });
+
 });
